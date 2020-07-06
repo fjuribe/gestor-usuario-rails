31cf94562c8031451e22b30c76a56475ae6a495c
fjuribe <fjuribeui@gmail.com>
2020-07-06 15:18:18 -0400

    Mejorando el modulo de session de la app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,17 @@ class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token, only: :new_session 
 
 	def login
-		
+		@user=User.new
 	end
 
 
     def new_session
     	#@user=User.find_by(email: params[:email])
-    	@user=User.find_by_email(params[:email])
+    	@user=User.find_by_email(user_params[:email])
 
     	if @user.nil?
     		redirect_to login_path, notice: 'El usuario no existe'
-    	elsif @user.valid_password?(params[:password])
+    	elsif @user.valid_password?(user_params[:password])
     		sign_in(:user, @user)
     		redirect_to :root,notice: 'Inicio de session iniciada correctamente'
     	else
@@ -24,5 +24,11 @@ class UsersController < ApplicationController
 	def logout
 		sign_out
 		redirect_to  :root,notice: 'Has cerrado session perfectamente'
+	end
+
+
+	private
+	def user_params
+		params.require(:user).permit(:email,:password)
 	end
 end
