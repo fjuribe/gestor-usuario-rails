25efb2b3b6817cb0671b6b6af2cd0a168d52973f
fjuribe <fjuribeui@gmail.com>
2020-07-08 16:53:18 -0400

    Terminando controlador del api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 	skip_before_action :verify_authenticity_token 
+	before_action :set_user, only:[:update,:show,:destroy]
+
 	def index
        @users=User.all
        render json: @users.as_json(only: [:name,:email])
@@ -16,8 +18,37 @@ class Api::V1::UsersController < ApplicationController
 	end
 
 
+	def show
+		@user=User.find_by_id(params[:id])
+		if @user
+			render json: @user
+		else
+		    render json:{error: "Usuario no encontrado"}, status: :not_found #404			
+		end
+	end
+
+    def update
+    	if @user.update(user_params)
+    		render json: @user
+    	else
+    		render json: @user.errors,status: :unprocessable_entity
+    	end
+    end
+
+
+    def destroy
+    	@user.destroy
+    	render json:{resultado: "Usuario eliminado correctamente"}, status: :no_content
+    end
+
+
+
     private
+	def set_user
+		@user=User.find_by_id(params[:id])
+	end
+
 	def user_params
-		params.require(:user).permit(:name,:email,:password)
+		params.require(:user).permit(:id,:name,:email,:password)
 	end
 end
